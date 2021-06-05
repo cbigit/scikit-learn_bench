4ab16e43fb9a1814fcffd60cb6f2bef2699feb10
Chris <7441139+cbigit@users.noreply.github.com>
2021-06-04 17:03:52 -0700

    Create prerun.sh

---
+++ prerun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#https://medium.com/intel-analytics-software/leverage-intel-optimizations-in-scikit-learn-f562cb9d5544
+#https://github.com/intel/scikit-learn-intelex
+
+python3 -m pip install -r sklearn_bench/requirements.txt
