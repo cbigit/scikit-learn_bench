962e113adb79dd9e68a20e0614be6778a453b512
Chris <7441139+cbigit@users.noreply.github.com>
2021-06-09 14:19:06 -0700

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -45,5 +45,6 @@ fi
 
 #https://medium.com/intel-analytics-software/leverage-intel-optimizations-in-scikit-learn-f562cb9d5544
 #https://github.com/intel/scikit-learn-intelex
+#python3 -m pip install -r sklearn_bench/requirements.txt
 
-python3 -m pip install -r sklearn_bench/requirements.txt
+conda install scikit-learn-intelex -c conda-forge
