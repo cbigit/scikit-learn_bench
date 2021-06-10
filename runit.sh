e52460a18a92adeea545bd44f67bd3c906e85989
Chris <7441139+cbigit@users.noreply.github.com>
2021-06-10 10:04:47 -0700

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -19,3 +19,13 @@ python runner.py --config configs/xgboost/xgb_cpu_additional_config.json
 
 #Start 
 #/opt/intel/oneapi/vtune/latest/bin64/vtune-backend --web-port=6006 --alloww-remote-ui
+
+#apt install -y linux-tools-common
+#You may need to install the following packages for this specific kernel:
+#apt install -y linux-tools-4.14.214-160.339.amzn2.x86_64
+#apt install -y linux-cloud-tools-4.14.214-160.339.amzn2.x86_64
+
+#You may also want to install one of the following packages to keep up to date:
+#apt install -y linux-tools-160.339.amzn2.x86_64
+#apt install -y linux-cloud-tools-160.339.amzn2.x86_64
+    
