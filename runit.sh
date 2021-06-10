36d64acc23b1c9f9f37eee5055414f997b24fc78
Chris <7441139+cbigit@users.noreply.github.com>
2021-06-10 06:23:06 -0700

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -4,4 +4,15 @@ echo "Installing intel scikit-learn-intelex"
 conda install -y scikit-learn-intelex -c conda-forge
 
 echo "Running configs/svm/svc_proba_sklearn.json"
+
+#Optimized sklearn
 python runner.py --config configs/svm/svc_proba_sklearn.json
+
+#Non-Optimized sklearn
+python runner.py --config configs/svm/svc_proba_sklearn.json --no-intel-optimized
+
+#XGBoost First
+python runner.py --config configs/xgboost/xgb_cpu_main_config.json
+
+#XGBoost Additional
+python runner.py --config configs/xgboost/xgb_cpu_additional_config.json
