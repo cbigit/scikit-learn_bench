9bb569842fe6ee86ed2cc9e924518b1d60a30f7e
Chris <7441139+cbigit@users.noreply.github.com>
2021-06-14 12:01:00 -0700

    Update runit.sh

--- runit.sh
+++ runit.sh
@@ -3,23 +3,29 @@
 echo "Installing intel scikit-learn-intelex"
 conda install -y scikit-learn-intelex -c conda-forge
 
-echo "Running configs/svm/svc_proba_sklearn.json"
+echo "Running configs/svm/*.json on:"$model
+
+#Determine CPU
+model_string=$(lscpu | grep 'Model name')
+model=$(echo $model_string | awk '{print $6}')
+echo "Running configs/svm/*.json on:"$model
 
 #Optimized sklearn
 python runner.py --config configs/svm/svc_proba_sklearn.json --report 
-#python /cnvrg/report_generator/report_generator.py --result-files /cnvrg/results.json --report-file /cnvrg/report.xlsx --generation-config configs/svm/svc_proba_sklearn.json --merging none
+##python /cnvrg/report_generator/report_generator.py --result-files /cnvrg/results.json --report-file /cnvrg/report1.xlsx --generation-config configs/svm/svc_proba_sklearn.json --merging full
+mv results.json.xlsx results-$model-svc_proba_sklearn-optimized.xlsx
 
 #Non-Optimized sklearn
 python runner.py --config configs/svm/svc_proba_sklearn.json --no-intel-optimized --report 
-#python /cnvrg/report_generator/report_generator.py --result-files /cnvrg/results.json --report-file /cnvrg/report.xlsx --generation-config configs/svm/svc_proba_sklearn.json --merging full
+mv results.json.xlsx results-$model-svc_proba_sklearn-non-optimized.xlsx
 
 #XGBoost First
 python runner.py --config configs/xgboost/xgb_cpu_main_config.json --report 
-#python /cnvrg/report_generator/report_generator.py --result-files /cnvrg/results.json --report-file /cnvrg/report.xlsx --generation-config configs/xgboost/xgb_cpu_main_config.json --merging full
+mv results.json.xlsx results-$model-xgboost-optimized.xlsx
 
 #XGBoost Additional
 python runner.py --config configs/xgboost/xgb_cpu_additional_config.json --report 
-#python /cnvrg/report_generator/report_generator.py --result-files /cnvrg/results.json --report-file /cnvrg/report.xlsx --generation-config configs/xgb_cpu_additional_config.json --merging full
+mv results.json.xlsx results-$model-xgboost-xgb_cpu_additional-optimized.xlsx
 
 #Start 
 #/opt/intel/oneapi/vtune/latest/bin64/vtune-backend --web-port=6006 --allow-remote-ui --data-directory=/cnvrg/vtune-results
@@ -32,4 +38,3 @@ python runner.py --config configs/xgboost/xgb_cpu_additional_config.json --repor
 #You may also want to install one of the following packages to keep up to date:
 #apt install -y linux-tools-160.339.amzn2.x86_64
 #apt install -y linux-cloud-tools-160.339.amzn2.x86_64
-    
